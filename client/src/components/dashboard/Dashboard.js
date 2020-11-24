3bc5b8b7164d807f73763dc9498d1858ee2b89d2
gttdo <vinces.gerardo@gmail.com>
2020-11-24 08:59:54 -0800

    modified: dashboardActions, Education, Experience, AddEducation, AddExperience, auth(reducer), profile(route)

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -5,9 +5,15 @@ import { connect } from 'react-redux';
 import Spinner from '../layout/Spinner';
 import DashboardActions from './DashboardActions';
 import Experience from './Experience';
-import { getCurrentProfile } from '../../actions/profile';
+import Education from './Education';
+import { deleteAccount, getCurrentProfile } from '../../actions/profile';
 
-const Dashboard = ({ getCurrentProfile, auth: { user }, profile: { profile, loading} }) => {
+const Dashboard = ({ 
+    getCurrentProfile,
+    deleteAccount,
+    auth: { user },
+    profile: { profile, loading} 
+    }) => {
     useEffect(() => {
         getCurrentProfile();
     }, []);
@@ -27,6 +33,14 @@ const Dashboard = ({ getCurrentProfile, auth: { user }, profile: { profile, load
                     <Fragment>
                         <DashboardActions/>
                         <Experience experience={profile.experience}/>
+                        <Education education={profile.education}/>
+
+                        <div className="my-2">
+                            <button className="btn btn-danger" onClick={() => deleteAccount()}>
+                                <i className="fas fa-user-minus"></i> {' '}
+                                Delete My Account
+                            </button>
+                        </div>
                     </Fragment>
                     ) : (
                     <Fragment>
@@ -42,6 +56,7 @@ const Dashboard = ({ getCurrentProfile, auth: { user }, profile: { profile, load
 
 Dashboard.propTypes = {
     getCurrentProfile: PropTypes.func.isRequired,
+    deleteAccount: PropTypes.func.isRequired,
     auth: PropTypes.object.isRequired,
     profile: PropTypes.object.isRequired
 };
@@ -52,4 +67,4 @@ const mapStateToProps = state => ({
 });
 
 
-export default connect(mapStateToProps, { getCurrentProfile })(Dashboard);
+export default connect(mapStateToProps, { getCurrentProfile, deleteAccount })(Dashboard);
--- client/src/components/profile-forms/AddEducation.js
+++ client/src/components/profile-forms/AddEducation.js
@@ -24,37 +24,37 @@ const AddEducation = ({ addEducation, history}) => {
 
     return (
         <Fragment>
-            <h1 class="large text-primary">
+            <h1 className="large text-primary">
             Add Your Education
             </h1>
-            <p class="lead">
-                <i class="fas fa-code-branch"></i> Add any school or bootcamp that you have attended.
+            <p className="lead">
+                <i className="fas fa-code-branch"></i> Add any school or bootcamp that you have attended.
             </p>
             <small>* = required field</small>
-            <form class="form" onSubmit={e => {
+            <form className="form" onSubmit={e => {
                 e.preventDefault();
                 addEducation(formData, history);
             }}>
-                <div class="form-group">
+                <div className="form-group">
                     <input type="text" placeholder="* School or Bootcamp" name="school" value={school} onChange={e => onChange(e)} required />
                 </div>
-                <div class="form-group">
+                <div className="form-group">
                     <input type="text" placeholder="* Degree or Certificate" name="degree" value={degree} onChange={e => onChange(e)} required />
                 </div>
-                <div class="form-group">
+                <div className="form-group">
                     <input type="text" placeholder="Field of Study" name="fieldofstudy" value={fieldofstudy} onChange={e => onChange(e)} />
                 </div>
-                <div class="form-group">
+                <div className="form-group">
                     <h4>From Date</h4>
                     <input type="date" name="from" value={from} onChange={e => onChange(e)}/>
                 </div>
-                <div class="form-group">
+                <div className="form-group">
                     <p><input type="checkbox" name="current" checked={current} value={current} onChange={e => {
                         setFormData({ ...formData, current: !current});
                         toggleDisabled(!toDateDisabled);
-                    }} /> {' '}Current Job</p>
+                    }} /> {' '}Currently Enrolled</p>
                 </div>
-                <div class="form-group">
+                <div className="form-group">
                     <h4>To Date</h4>
                     <input 
                         type="date" 
@@ -64,7 +64,7 @@ const AddEducation = ({ addEducation, history}) => {
                         disabled={toDateDisabled ? 'disabled' : ''}
                     />
                 </div>
-                <div class="form-group">
+                <div className="form-group">
                     <textarea
                         name="description"
                         cols="30"
@@ -74,8 +74,8 @@ const AddEducation = ({ addEducation, history}) => {
                         onChange={e => onChange(e)}
                     ></textarea>
                 </div>
-                <input type="submit" class="btn btn-primary my-1" />
-                <a class="btn btn-light my-1" href="dashboard.html">Go Back</a>
+                <input type="submit" className="btn btn-primary my-1" />
+                <Link className="btn btn-light my-1" to="/dashboard">Go Back</Link>
             </form>
         </Fragment>
     )
